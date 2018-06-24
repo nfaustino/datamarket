452620b1c9a9e250c9503ebac91a95250c240f6d
nfaustino <nuno.faustino@gmail.com>
2018-06-24 16:53:52 +0100

    Update dm_WorldPopulation.py

--- datamarket/spiders/dm_WorldPopulation.py
+++ datamarket/spiders/dm_WorldPopulation.py
@@ -7,4 +7,8 @@ Created on Sat Jun 23 23:56:08 2018
 
 from scrapy.spiders import BaseSpider
 from scrapy.selector import HtmlXPathSelector
-from datamarket.items import DatamarketSampleItem
+from datamarket.items import *
+import pandas as pd
+
+
+df = pd.read_csv('https://datamarket-api.qlik.com/api/v6/table.csv?ds=516m')
